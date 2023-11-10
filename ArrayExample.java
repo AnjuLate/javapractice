064f79a413b24a40e05b7a7670068ca8fbb62e6b
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-10 14:38:24 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,10 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+
+        int index= 3;
+        int retrieve = a[index];
+
+        System.out.println(retrieve);
+    }
+}
