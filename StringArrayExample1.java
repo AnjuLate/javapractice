e860114b277b722551d9bd142a86ae9402a138c1
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-10 14:59:16 +0530

    Add files via upload

---
+++ StringArrayExample1.java
@@ -0,0 +1,8 @@
+public class StringArrayExample1 {
+    public static void main(String[] args) {
+        String[] stringArray = {"java", "python", "Ruby", "Go"};
+        for (int i = 0; i < stringArray.length; i++) {
+            System.out.println(stringArray[i]);
+        }
+    }
+}
