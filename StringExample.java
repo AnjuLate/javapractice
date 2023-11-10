4d56dcee31ac5230cf9609904786988f814ab60f
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-10 14:40:34 +0530

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,10 @@
+public class StringExample {
+    public static void main(String[] args) {
+    
+        String myString = "Welcome to Java Programming!";
+        int index = 5;
+        char retrieve = myString.charAt(index);
+
+        System.out.println(retrieve);
+    }
+}
