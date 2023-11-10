cd24c5c9cfec5481b07246f06bd4c3c674775442
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-10 14:45:49 +0530

    Add files via upload

---
+++ ByteToInt.java
@@ -0,0 +1,8 @@
+public class ByteToInt {
+    public static void main(String[] args) {
+        byte byteV = 42;
+        int intV = (int) byteV;
+        System.out.println("Original byte value: " + byteV);
+        System.out.println("Converted int value: " + intV);
+    }
+}
