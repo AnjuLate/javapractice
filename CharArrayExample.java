8353f1ae15af4d3f51ddafa22405524a928a317e
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-10 14:36:22 +0530

    Add files via upload

---
+++ CharArrayExample.java
@@ -0,0 +1,10 @@
+public class CharArrayExample {
+    public static void main(String[] args) {
+        char[] charArray = {'A', 'B', 'C', 'D'};
+
+        System.out.println("Char Array:");
+        for (char element : charArray) {
+            System.out.println(element);
+        }
+    }
+}
