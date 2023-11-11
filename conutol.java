70ba629759a8a47fb3ef8897820c53463b619d2a
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-11 18:32:39 +0530

    Add files via upload

---
+++ conutol.java
@@ -0,0 +1,14 @@
+
+public class conutol
+{
+   public static void main(String[] args)
+   {
+      String strU = "ANJALI";
+      
+      String strLower = strU.toLowerCase();
+      System.out.println(strLower);
+   }
+}
+
+
+ 
