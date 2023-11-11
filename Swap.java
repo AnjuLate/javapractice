c033e1b2dbb35f5bd02a5d536d57000fb12ca587
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-11 17:34:58 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,15 @@
+class Swap   
+{  
+    public static void main(String a[])   
+    {   
+          
+        int x=10;
+        int y=20;
+
+        x = x + y;   
+        y = x - y;   
+        x = x - y;   
+        System.out.println(x);    
+        System.out.println(y);   
+    }   
+}  
