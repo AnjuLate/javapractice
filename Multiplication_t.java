d44751fbc41aaf56f6b89e3145da5ee0a87ce2e3
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-17 17:59:45 +0530

    Add files via upload

---
+++ Multiplication_t.java
@@ -0,0 +1,10 @@
+public class Multiplication_t{
+    public static void main(String[] args) {
+        int number = 5;
+        int size = 10; 
+        
+        for (int i = 1; i <= size; i++) {
+            System.out.println((number * i));
+        }
+    }
+}
