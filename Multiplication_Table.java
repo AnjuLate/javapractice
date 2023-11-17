aedd3ef96a9fc3536bc67bd9235a794704a18e11
AnjuLate <121994547+AnjuLate@users.noreply.github.com>
2023-11-17 18:02:47 +0530

    Add files via upload

---
+++ Multiplication_Table.java
@@ -0,0 +1,10 @@
+public class Multiplication_Table{
+    public static void main(String[] args) {
+        int number = 10;
+        int size = 10; 
+        
+        for (int i = 1; i <= size; i++) {
+            System.out.println((number * i));
+        }
+    }
+}
